5dde4f7c04423ce2c2b139fdebb463f30e99d49e
Dhruv Bhardwaj <11dhruv.b@gmail.com>
2026-05-25 00:31:12 -0400

    Send Firebase token GraphQL requests

--- frontend/src/config/apolloClient.js
+++ frontend/src/config/apolloClient.js
@@ -1,12 +1,24 @@
-import {ApolloClient, InMemoryCache, HttpLink} from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from "@apollo/client";
 
 const httpLink = new HttpLink({
-    uri: "http://localhost:4000/graphql",
+  uri: "http://localhost:4000/graphql",
+});
+
+const authLink = new ApolloLink((operation, forward) => {
+    const token = localStorage.getItem("firebaseToken");
+
+    operation.setContext({
+        headers:{
+            authorization: token ? `Bearer ${token}` : "",
+        },
+    });
+
+    return forward(operation);
 });
 
 const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 export default client;
