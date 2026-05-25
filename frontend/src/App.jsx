df8d78c68f4de36323b193201df4acdaa8720389
Dhruv Bhardwaj <11dhruv.b@gmail.com>
2026-05-25 01:12:46 -0400

    Persist Firebase login after refresh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,31 +2,57 @@ import { useState } from "react";
 import AddBookForm from "./components/AddBookForm";
 import BookList from "./components/BookList";
 import Login from "./components/Login";
+import { useEffect } from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "./config/firebase";
 
-const App =() => {
+const App = () => {
   const [user, setUser] = useState(null);
+  const [authLoading, setAuthLoading] = useState(true);
 
-  const handleLogout = () =>{
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      if (currentUser) {
+        const token = await currentUser.getIdToken();
+        localStorage.setItem("firebaseToken", token);
+        setUser(currentUser);
+      } else {
+        localStorage.removeItem("firebaseToken");
+        setUser(null);
+      }
+
+      setAuthLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleLogout = async () => {
+    await signOut(auth);
     localStorage.removeItem("firebaseToken");
-    setUser(null)
+    setUser(null);
+  };
+
+  if (authLoading) {
+    return <p>Checking authentication...</p>;
   }
   return (
     <div>
       <h1>Mini Book Notes</h1>
-      
+
       {user ? (
         <div>
-          <p>Logged in as: user.email</p>
+          <p>Logged in as: {user.email}</p>
           <button onClick={handleLogout}>Logout</button>
         </div>
       ) : (
         <Login setUser={setUser} />
       )}
       <AddBookForm />
-      
+
       <BookList />
     </div>
   );
-}
+};
 
 export default App;
